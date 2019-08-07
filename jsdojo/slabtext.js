9ba17d4904a2cd931c823ad426f9ad3160d556e1
g001561 <g001561@pc-001561-1.local>
2019-08-07 11:19:31 +0900

    styling edits

--- jsdojo/slabtext.js
+++ jsdojo/slabtext.js
@@ -1,21 +1,21 @@
-(function () {
-    "use strict";
-    $( document ).ready(function() {
+(function() {
+    'use strict';
+    $(document).ready(() => {
         kintone.events.on('app.record.detail.show', function(event) {
-            var textField = kintone.app.record.getFieldElement('Text');
+            const textField = kintone.app.record.getFieldElement('Text');
             console.log(textField);
-            var text = event.record.Text.value;
-            var slab0 = "<span class='slabtext'>",
-                span0 = "</span>",
+            const text = event.record.Text.value;
+            const slab0 = '<span class=\'slabtext\'>',
+                span0 = '</span>',
                 txt0 = [text];    
             $(textField).html(slab0 + txt0.join(span0 + slab0) + span0).slabText();
 
         });
         kintone.events.on('app.record.index.show', function(event) {
-            var header = kintone.app.getHeaderSpaceElement();
-            var hElem =  "<span class='slabtext'>",
-            hspan = "</span>",
-            htext = ["Insert Reminder Here!"];
+            const header = kintone.app.getHeaderSpaceElement();
+            const hElem = '<span class=\'slabtext\'>',
+                hspan = '</span>',
+                htext = ['Insert Reminder Here!'];
             $(header).html(hElem + htext.join(hspan + hElem) + hspan).slabText();
         });
     });
