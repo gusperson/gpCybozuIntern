0bb850dafacc072154cd2073d71478860c715b3c
g001561 <g001561@pc-001561-1.local>
2019-08-07 11:09:13 +0900

    styling changes, eslinted the code and make small edits

--- jstree/src/js/config.js
+++ jstree/src/js/config.js
@@ -1,130 +1,125 @@
+/* eslint-disable prefer-arrow-callback */
 jQuery.noConflict();
 (function($, PLUGIN_ID) {
-  'use strict';
-  var $form = $('.js-submit-settings');
-  var $folder = $('#select_dropdown');
-  var $text = $('#select_text');
-  var $cancelButton = $('.js-cancel-button');
-  var $root = $('.js-text-message');
-  var $viewname = $('.js-view-name');
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-  var buttonbool = false;
-  document.getElementById('viewmaker').onclick = function() {
-    if ($viewname === '') {
-      window.alert('Please enter a folder view name.');
-    } else {
-      buttonbool = true;
-      var app = {
-        'app': kintone.app.getId(),
-      };
-      kintone.api(kintone.api.url('/k/v1/preview/app/views', true), 'GET', app, function(view) {
-        view.app = kintone.app.getId();
-        var maxindex = -1;
-        var firstview = true;
-        for (var numb in view.views) {
-          if (view.views[numb].index > maxindex) {
-            maxindex = view.views[numb].index + 1;
-            firstview = false;
-          }
+    'use strict';
+    const $form = $('.js-submit-settings');
+    const $folder = $('#select_dropdown');
+    const $text = $('#select_text');
+    const $cancelButton = $('.js-cancel-button');
+    const $root = $('.js-text-message');
+    const $viewname = $('.js-view-name');
+    let config = kintone.plugin.app.getConfig(PLUGIN_ID);
+    let buttonbool = false;
+    document.getElementById('viewmaker').onclick = function() {
+        if ($viewname === '') {
+            window.alert('Please enter a folder view name.');
+        } else {
+            buttonbool = true;
+            const app = {
+                'app': kintone.app.getId(),
+            };
+            kintone.api(kintone.api.url('/k/v1/preview/app/views', true), 'GET', app, (view) => {
+                view.app = kintone.app.getId();
+                let maxindex = -1;
+                let firstview = true;
+                for (const numb in view.views) {
+                    if ((view.views.hasOwnProperty(numb))) {
+                        if (view.views[numb].index > maxindex) {
+                            maxindex = view.views[numb].index + 1;
+                            firstview = false;
+                        }
+                    }
+                }
+                if (firstview) {
+                    maxindex = 0;
+                }
+                view.views[$viewname.val()] = {
+                    'filterCond': '',
+                    'name': $viewname.val(),
+                    'type': 'CUSTOM',
+                    'html': '<ul id="tree"></ul>',
+                    'index': maxindex,
+                };
+                kintone.api(kintone.api.url('/k/v1/preview/app/views', true), 'PUT', view, (resp) => {
+                    // success
+                    window.alert('View Created!');
+                }, function(error) {
+                    window.alert('Please enter a folder view name.');
+                    // error
+                    console.log(error);
+                });
+                // success
+            }, function(error) {
+                // error
+                console.log(error);
+            });
         }
-        if (firstview) {
-          maxindex = 0;
+    };
+    KintoneConfigHelper.getFields().then(function(resp) {
+        for (let i = 0; i < resp.length; i++) {
+            switch (resp[i].type) {
+            case 'DROP_DOWN':
+                var opt = $('<option>'); document.createElement('option');
+                var text = document.createTextNode(resp[i].label);
+                opt.attr('value', resp[i].code);//opt.setAttribute('value', resp[i].code);
+                opt.append(text);
+                $folder.append(opt);
+                break;
+            case 'SINGLE_LINE_TEXT':
+                var opt = $('<option>'); //document.createElement('option');
+                var text = document.createTextNode(resp[i].label);
+                opt.attr('value', resp[i].code); //opt.setAttribute('value', resp[i].code);
+                opt.append(text);
+                $text.append(opt);
+                break;
+            default:
+                break;
+            }
         }
-        view.views[$viewname.val()] = {
-          'id': 74726565,
-          'filterCond': '',
-          'name': $viewname.val(),
-          'type': 'CUSTOM',
-          'html': '<ul id=tree></ul>',
-          'index': maxindex,
-        };
-        kintone.api(kintone.api.url('/k/v1/preview/app/views', true), 'PUT', view, function(resp) {
-        // success
-          window.alert('View Created!');
-          var newSettings = {
-            apps: [
-              {
-                'app': kintone.app.getId()
-              }
-            ]
-          };
-        }, function(error) {
-          window.alert('Please enter a folder view name.');
-          // error
-          console.log(error);
-        });
-      // success
-      }, function(error) {
-      // error
+        if (config.dropdown) {
+            $folder.val(config.dropdown);
+        }
+        if (config.text) {
+            $text.val(config.text);
+        }
+    }, function(error) {
+        // error
         console.log(error);
-      });
+    });
+    if (config.root) {
+        $root.val(config.root);
     }
-  };
-  KintoneConfigHelper.getFields().then(function(resp) {
-    for (var i = 0; i < resp.length; i++) {
-      switch (resp[i].type) {
-        case 'DROP_DOWN':
-          var opt = document.createElement('option');
-          var text = document.createTextNode(resp[i].label);
-          opt.setAttribute('value', resp[i].code);
-          opt.append(text);
-          $folder.append(opt);
-          break;
-        case 'SINGLE_LINE_TEXT':
-          var opt = document.createElement('option');
-          var text = document.createTextNode(resp[i].label);
-          opt.setAttribute('value', resp[i].code);
-          opt.append(text);
-          $text.append(opt);
-          break;
-        default:
-          break;
-      }
+    if (config.viewname) {
+        $viewname.val(config.viewname);
     }
-    if (config.dropdown) {
-      $folder.val(config.dropdown);
-    }
-    if (config.text) {
-      $text.val(config.text);
-    }
-  }, function(error) {
-    // error
-    console.log(error);
-  });
-  if (config.root) {
-    $root.val(config.root);
-  }
-  if (config.viewname) {
-    $viewname.val(config.viewname);
-  }
-  $form.on('submit', function(e) {
-    if (buttonbool || config.viewname) {
-      e.preventDefault();
-      if (buttonbool) {
-        config = {
-          'dropdown': $folder.val(),
-          'root': $root.val(),
-          'text': $text.val(),
-          'viewname': $viewname.val(),
-        };
-      } else {
-        config = {
-          'dropdown': $folder.val(),
-          'root': $root.val(),
-          'text': $text.val(),
-          'viewname': config.viewname
-        };
-      }
-      kintone.plugin.app.setConfig(config, function() {
-        alert('The plug-in settings have been saved. Please update the app!');
-        window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
-      });
-    } else {
-      window.alert('Please press the button to create a Folder View page!');
-      return false;
-    }
-  });
-  $cancelButton.on('click', function() {
-    window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
-  });
+    $form.on('submit', function(e) {
+        if (buttonbool || config.viewname) {
+            e.preventDefault();
+            if (buttonbool) {
+                config = {
+                    'dropdown': $folder.val(),
+                    'root': $root.val(),
+                    'text': $text.val(),
+                    'viewname': $viewname.val(),
+                };
+            } else {
+                config = {
+                    'dropdown': $folder.val(),
+                    'root': $root.val(),
+                    'text': $text.val(),
+                    'viewname': config.viewname
+                };
+            }
+            kintone.plugin.app.setConfig(config, () => {
+                alert('The plug-in settings have been saved. Please update the app!');
+                window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
+            });
+        } else {
+            window.alert('Please press the button to create a Folder View page!');
+            return false;
+        }
+    });
+    $cancelButton.on('click', () => {
+        window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
+    });
 })(jQuery, kintone.$PLUGIN_ID);
--- jstree/src/js/desktop.js
+++ jstree/src/js/desktop.js
@@ -1,61 +1,61 @@
 jQuery.noConflict();
 (function($, PLUGIN_ID) {
-  'use strict';
-  kintone.events.on('app.record.index.show', function(event) {
-    if (!!(document.getElementById('tree'))) {
-      var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-      var root = config.root;
-      var folder = config.dropdown;
-      var textField = config.text;
-      var body = {
-        'app': kintone.app.getId()
-      };
-      $('#tree').jstree({
-        'core': {
-          'check_callback': true,
-          'themes': {'variant': 'large'},
-          'data': [
-            {
-              'text': root,
-              'expand_selected_onload': 'false',
-              'id': 'root',
-              'numb': -1,
-              'children': [
-              ]
-            }
-          ]
+    'use strict';
+    kintone.events.on('app.record.index.show', function(event) {
+        if (document.getElementById('tree')) {
+            const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+            const root = config.root;
+            const folder = config.dropdown;
+            const textField = config.text;
+            const body = {
+                'app': kintone.app.getId()
+            };
+            $('#tree').jstree({
+                'core': {
+                    'check_callback': true,
+                    'themes': {'variant': 'large'},
+                    'data': [
+                        {
+                            'text': root,
+                            'expand_selected_onload': 'false',
+                            'id': 'root',
+                            'numb': -1,
+                            'children': [
+                            ]
+                        }
+                    ]
+                }
+            });
+            kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', body, function(resp) {
+                for (const prop in resp.properties) {
+                    if (folder === prop) {
+                        for (const option in resp.properties[prop].options) {
+                            $('#tree').jstree(true).create_node('#root', {id: option, text: option, numb: -1});
+                        }
+                    }
+                }
+                // success
+            }, function(error) {
+                // error
+                console.log(error);
+            });
+            kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body, function(resp) {
+                for (let i = 0; i < resp.records.length; i++) {
+                    const recNum = resp.records[i].$id.value; //const recNum = resp.records[i].Record_number.value;
+                    const name = resp.records[i].Text.value;
+                    const department = resp.records[i][folder].value;
+                    $('#tree').jstree(true).create_node('#' + department, {text: name, numb: recNum, icon: false});
+                }
+            }, function(error) {
+                console.log(error);
+            });
+            $('#tree').on('select_node.jstree', function(e, data) {
+                if (data.node.original.numb !== -1) {
+                    const origin = window.location.origin;
+                    const pathname = window.location.pathname;
+                    window.open(origin + pathname + 'show#record=' + data.node.original.numb);
+                }
+            });
         }
-      });
-      kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', body, function(resp) {
-        for (var prop in resp.properties) {
-          if (folder === prop) {
-            for (var option in resp.properties[prop].options) {
-              $('#tree').jstree(true).create_node('#root', {id: option, text: option, numb: -1});
-            }
-          }
-        }
-        // success
-      }, function(error) {
-        // error
-        console.log(error);
-      });
-      kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body, function(resp) {
-        for (var i = 0; i < resp.records.length; i++) {
-          var recNum = resp.records[i].Record_number.value;
-          var name = resp.records[i].Text.value;
-          var department = resp.records[i][folder].value;
-          $('#tree').jstree(true).create_node('#' + department, {text: name, numb: recNum, icon: false});
-        }
-      }, function(error) {
-        console.log(error);
-      });
-      $('#tree').on('select_node.jstree', function(e, data) {
-        if (data.node.original.numb !== -1) {
-          var origin = window.location.origin;
-          var pathname = window.location.pathname;
-          window.open(origin + pathname + 'show#record=' + data.node.original.numb);
-        }
-      });
-    }
-  });
+    });
 })(jQuery, kintone.$PLUGIN_ID);
