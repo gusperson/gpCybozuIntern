79456fde3075df4e52faa5672fee94650e5664d2
g001561 <g001561@pc-001561-1.local>
2019-08-07 13:16:25 +0900

    WIP string escaping for header space

--- jsdojo/slabtext.js
+++ jsdojo/slabtext.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     $(document).ready(() => {
-        kintone.events.on('app.record.detail.show', function(event) {
+        kintone.events.on('app.record.detail.show', (event) => {
             const textField = kintone.app.record.getFieldElement('Text');
             console.log(textField);
             const text = event.record.Text.value;
@@ -10,12 +10,24 @@
                 txt0 = [text];    
             $(textField).html(slab0 + txt0.join(span0 + slab0) + span0).slabText();
         });
+        // eslint-disable-next-line prefer-arrow-callback
         kintone.events.on('app.record.index.show', function(event) {
             const header = kintone.app.getHeaderSpaceElement();
+            const cleaned = sanitizeStr(header.text());
             const hElem = '<span class=\'slabtext\'>',
                 hspan = '</span>',
-                htext = ['Insert Reminder Here!'];
+                htext = ['Insert Reminder Here!'];   
             $(header).html(hElem + htext.join(hspan + hElem) + hspan).slabText();
         });
+        function sanitizeStr(str) {
+            let tmpstr = str;
+            tmpstr = tmpstr.replace(/&/g, '&amp;');
+            tmpstr = tmpstr.replace(/</g, '&lt;');
+            tmpstr = tmpstr.replace(/>/g, '&gt;');
+            tmpstr = tmpstr.replace(/"/g, '&quot;');
+            tmpstr = tmpstr.replace(/'/g, '&#39;');
+            return tmpstr;
+        }
+
     });
 })();
